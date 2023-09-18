73201c980c081e1312a3ac0e9b6005d5c55a6100
Andrei5223 <andrei.camilotto@gmail.com>
2023-09-18 17:55:21 -0300

    Bootstrap
    Introdução ao bootstrap

---
+++ Programação II/Aula 5/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.listen(3001, () => console.log('Servidor na porta 3001.'));
+app.use(express.static(`${__dirname}/public`));
+
+// Retorna uma pagina html na request
+app.get('/', (request, response) => {
+    response.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/pessoa/:id', (request, response) => {
+    const nome = request.body.nome;
+    console.log('Recebi uma request do tipo POST');
+    response.send(`Olá ${nome}!`);
+});
