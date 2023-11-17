d7ade455aa60ee033cde82a5df8c2a3e629455f9
Andrei C. Camilotto <andrei.camilotto@gmail.com>
2023-11-17 15:06:50 -0300

    Reset integrador

--- Semestre 4/Programação II/LAB08 copy/frontend/src/components/Subtitulo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SubTitulo(props) {
-    return (
-        <h2>{props.mensagem}</h2>
-    )
-}
-
-export default SubTitulo;
--- Semestre 4/Programação II/LAB08 copy/frontend/src/components/Titulo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Titulo(props) {
-    return (
-        <h1>{props.mensagem}</h1>
-    )
-}
-
-export default Titulo;
